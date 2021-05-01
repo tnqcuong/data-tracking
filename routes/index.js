a72c893de43926c36c45c4685f565c36bac7c4bf
Cuong Tran <tnqcuong@gmail.com>
2021-05-01 07:03:23 +0700

    update code

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,9 @@ const apiUrl = {
   candleStick: "/api/v3/klines",
 };
 
-const headersParams = { "Content-Type": "application/json" };
+const headersParams = {
+  "Content-Type": "application/json"
+};
 const params = {};
 
 function getExchangeInfo() {
@@ -22,70 +24,119 @@ function getExchangeInfo() {
   };
 
   axios(config)
-  .then((response) => {
-    console.log(response);
-    res = response.data;
-  }, (error) => {
-    console.log(error);
-  });
-  
+    .then((response) => {
+      console.log('===========================')
+      res = response.data;
+      let dataList = res.symbols;
+      let symbolList = [];
+      for (let i = 0; i < dataList.length; i++) {
+        if (dataList[i].quoteAsset == "USDT") {
+          symbolList.push(dataList[i]);
+        }
+      }
+
+      let result = [];
+
+      for (let j = 0; j < symbolList.length; j++) {
+        getCandleStickInfo(symbolList[j].symbol, '1m').then((res) => {
+          dataList = res.data;
+          for (let i = 0; i < dataList.length; i++) {
+            if (dataList[i][4] - dataList[i][1] > 0) {
+              let percentUp = ((dataList[i][4] - dataList[i][1]) / dataList[i][1]) * 100;
+              if (percentUp > 0) {
+
+                getCandleStickInfo(symbolList[j].symbol, '5m').then((res) => {
+                  let dataList1 = res.data;
+                  for (let i = 0; i < dataList1.length; i++) {
+                    if (dataList1[i][4] - dataList1[i][1] > 0) {
+                      let percentUp = ((dataList1[i][4] - dataList1[i][1]) / dataList1[i][1]) * 100;
+                      if (percentUp > 1) {
+
+                        getCandleStickInfo(symbolList[j].symbol, '15m').then((res) => {
+                          let dataList2 = res.data;
+                          for (let i = 0; i < dataList2.length; i++) {
+                            if (dataList2[i][4] - dataList2[i][1] > 0) {
+                              let percentUp = ((dataList2[i][4] - dataList2[i][1]) / dataList1[i][1]) * 100;
+                              if (percentUp > 1) {
+                                
+                                
+                                getCandleStickInfo(symbolList[j].symbol, '30m').then((res) => {
+                                  let dataList3 = res.data;
+                                  for (let i = 0; i < dataList3.length; i++) {
+                                    if (dataList3[i][4] - dataList3[i][1] > 0) {
+                                      let percentUp = ((dataList3[i][4] - dataList3[i][1]) / dataList1[i][1]) * 100;
+                                      if (percentUp > 2) {
+                                        console.log(symbolList[j].baseAsset + " : " + percentUp);
+                                        break;
+                                      }
+                                    }
+                                  }
+                                });
+                                
+                                //console.log(symbolList[j].baseAsset + " : " + percentUp);
+                                break;
+                              }
+                            }
+                          }
+                        });
+
+                        //console.log(symbolList[j].baseAsset + " : " + percentUp);
+                        break;
+                      }
+                    }
+                  }
+                });
+
+                // result.push(symbolList[j]);
+                // console.log(symbolList[j].baseAsset + " : " + percentUp);
+                break;
+              }
+            }
+          }
+        });
+      }
+    }, (error) => {
+      console.log(error.data.message);
+    });
+
   return res;
 }
 
-getExchangeInfo();
+async function getCandleStickInfo(symbolVal, time) {
+  
+  let payload = {
+    symbol: symbolVal,
+    interval: time,
+    limit: "1"
+  };
+  const params = new url.URLSearchParams(payload);
 
+  const config = {
+    method: "get",
+    url: baseUrl + apiUrl.candleStick + "?" + params.toString(),
+    headers: {
+      "Content-Type": "application/json"
+    },
+  };
+
+  try {
+    return await axios(config);
+  } catch (error) {
+    console.error(error.data.message)
+  }
+}
+
+getExchangeInfo();
+//setInterval(() => getExchangeInfo(), 60*1000);
+
+//setInterval(() => dataTracking(), 1*1000);
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "text" });
+  res.render("index", {
+    title: "text"
+  });
 });
 
 module.exports = router;
 
-// async function getCandleStickInfo(symbolVal) {
-//   let payload = { symbol: symbolVal, interval: "5m", limit: "1" };
-//   const params = new url.URLSearchParams(payload);
-
-//   const config = {
-//     method: "get",
-//     url: baseUrl + apiUrl.candleStick + "?" + params.toString(),
-//     headers: { "Content-Type": "application/json" },
-//   };
-
-//   let res = await axios(config);
-//   return res.data;
-// }
-
-// function mainFlow() {
-//   //console.clear();
-//   console.log("=====================");
-//   let symbolList = [];
-//   let result = [];
-//   let exchangeInfo = getExchangeInfo().then((res) => {
-//       let dataList = res.symbols;
-//       for (let i = 0; i < dataList.length; i++) {
-//         if (dataList[i].quoteAsset == "USDT") {
-//           symbolList.push(dataList[i]);
-//         }
-//       }
-
-//       for (let j = 0; j < symbolList.length; j++) {
-//         getCandleStickInfo(symbolList[j].symbol).then((res) => {
-//             dataList = res;
-//             for (let i = 0; i < dataList.length; i++) {
-//               if (dataList[i][4] - dataList[i][1] > 0) {
-//                 let percentUp =
-//                   ((dataList[i][4] - dataList[i][1]) / dataList[i][1]) * 100;
-//                 if (percentUp > 1) {
-//                   result.push(symbolList[j]);
-//                   console.log(symbolList[j].baseAsset + " : " + percentUp);
-//                   break;
-//                 }
-//               }
-//             }
-//         });
-//       }
-//   });
-// }
-
-// setInterval(() => mainFlow(), 60*1000);
