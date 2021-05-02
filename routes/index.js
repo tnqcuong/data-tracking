292944fa6468ff8ee3dceabf0a6da5336364ae3b
Cuong Tran <tnqcuong@gmail.com>
2021-05-02 11:52:10 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,7 @@ function getExchangeInfo() {
 
   axios(config)
     .then((response) => {
+      // console.clear();
       console.log('===========================')
       res = response.data;
       let dataList = res.symbols;
@@ -39,57 +40,65 @@ function getExchangeInfo() {
 
       for (let j = 0; j < symbolList.length; j++) {
         getCandleStickInfo(symbolList[j].symbol, '1m').then((res) => {
-          dataList = res.data;
-          for (let i = 0; i < dataList.length; i++) {
-            if (dataList[i][4] - dataList[i][1] > 0) {
-              let percentUp = ((dataList[i][4] - dataList[i][1]) / dataList[i][1]) * 100;
-              if (percentUp > 0) {
-
-                getCandleStickInfo(symbolList[j].symbol, '5m').then((res) => {
-                  let dataList1 = res.data;
-                  for (let i = 0; i < dataList1.length; i++) {
-                    if (dataList1[i][4] - dataList1[i][1] > 0) {
-                      let percentUp = ((dataList1[i][4] - dataList1[i][1]) / dataList1[i][1]) * 100;
-                      if (percentUp > 1) {
-
-                        getCandleStickInfo(symbolList[j].symbol, '15m').then((res) => {
-                          let dataList2 = res.data;
-                          for (let i = 0; i < dataList2.length; i++) {
-                            if (dataList2[i][4] - dataList2[i][1] > 0) {
-                              let percentUp = ((dataList2[i][4] - dataList2[i][1]) / dataList1[i][1]) * 100;
-                              if (percentUp > 1) {
-                                
-                                
-                                getCandleStickInfo(symbolList[j].symbol, '30m').then((res) => {
-                                  let dataList3 = res.data;
-                                  for (let i = 0; i < dataList3.length; i++) {
-                                    if (dataList3[i][4] - dataList3[i][1] > 0) {
-                                      let percentUp = ((dataList3[i][4] - dataList3[i][1]) / dataList1[i][1]) * 100;
-                                      if (percentUp > 2) {
-                                        console.log(symbolList[j].baseAsset + " : " + percentUp);
-                                        break;
+          if(res.data){
+            dataList = res.data;
+            for (let i = 0; i < dataList.length; i++) {
+              if (dataList[i][4] - dataList[i][1] > 0) {
+                let percentUp = ((dataList[i][4] - dataList[i][1]) / dataList[i][1]) * 100;
+                if (percentUp > 0 && !symbolList[j].baseAsset.includes('BEAR') && !symbolList[j].baseAsset.includes('BULL') && !symbolList[j].baseAsset.includes('UP') && !symbolList[j].baseAsset.includes('DOWN') && symbolList[j].baseAsset != 'BCHSV' && symbolList[j].baseAsset != 'STORM' && symbolList[j].baseAsset != 'ERD' && symbolList[j].baseAsset != 'XZC') {
+                  if(percentUp > 0.5){
+                    console.log('==1m==: ' + symbolList[j].baseAsset + " : " + percentUp);
+                  }
+                  getCandleStickInfo(symbolList[j].symbol, '5m').then((res) => {
+                    let dataList1 = res.data;
+                    for (let i = 0; i < dataList1.length; i++) {
+                      if (dataList1[i][4] - dataList1[i][1] > 0) {
+                        let percentUp = ((dataList1[i][4] - dataList1[i][1]) / dataList1[i][1]) * 100;
+                        if (percentUp > 0.5) {
+                          if(percentUp > 1){
+                            console.log('==5m==: ' + symbolList[j].baseAsset + " : " + percentUp);
+                          }
+                          getCandleStickInfo(symbolList[j].symbol, '15m').then((res) => {
+                            let dataList2 = res.data;
+                            for (let i = 0; i < dataList2.length; i++) {
+                              if (dataList2[i][4] - dataList2[i][1] > 0) {
+                                let percentUp = ((dataList2[i][4] - dataList2[i][1]) / dataList1[i][1]) * 100;
+                                if (percentUp > 1) {
+                                  if(percentUp > 1.5){
+                                    console.log('==15m==: ',symbolList[j].baseAsset);
+                                  }
+                                  
+                                  getCandleStickInfo(symbolList[j].symbol, '30m').then((res) => {
+                                    let dataList3 = res.data;
+                                    for (let i = 0; i < dataList3.length; i++) {
+                                      if (dataList3[i][4] - dataList3[i][1] > 0) {
+                                        let percentUp = ((dataList3[i][4] - dataList3[i][1]) / dataList1[i][1]) * 100;
+                                        if (percentUp > 2) {
+                                          console.log(symbolList[j].baseAsset + " : " + percentUp);
+                                          break;
+                                        }
                                       }
                                     }
-                                  }
-                                });
-                                
-                                //console.log(symbolList[j].baseAsset + " : " + percentUp);
-                                break;
+                                  });
+                                  
+                                  //console.log(symbolList[j].baseAsset + " : " + percentUp);
+                                  break;
+                                }
                               }
                             }
-                          }
-                        });
-
-                        //console.log(symbolList[j].baseAsset + " : " + percentUp);
-                        break;
+                          });
+  
+                          //console.log(symbolList[j].baseAsset + " : " + percentUp);
+                          break;
+                        }
                       }
                     }
-                  }
-                });
-
-                // result.push(symbolList[j]);
-                // console.log(symbolList[j].baseAsset + " : " + percentUp);
-                break;
+                  });
+  
+                  // result.push(symbolList[j]);
+                  // console.log(symbolList[j].baseAsset + " : " + percentUp);
+                  break;
+                }
               }
             }
           }
@@ -122,12 +131,12 @@ async function getCandleStickInfo(symbolVal, time) {
   try {
     return await axios(config);
   } catch (error) {
-    console.error(error.data.message)
+    //console.error(error.data.message)
   }
 }
 
-getExchangeInfo();
-//setInterval(() => getExchangeInfo(), 60*1000);
+// getExchangeInfo();
+setInterval(() => getExchangeInfo(), 60*1000);
 
 //setInterval(() => dataTracking(), 1*1000);
 
